a21f6723b8e99a2c46faa0bc5cfb0fb9de38afdb
awkward-3312 <betza_es77@hotmail.com>
2025-06-19 23:15:27 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
-      <ColorModeScript />
       <App />
     </ChakraProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
